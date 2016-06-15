11fd5eee30b152538042b130623c76138eb8d88c
Gleiver Gil <Gleiver Gil>
2016-06-15 19:20:46 -0430

    actas2

--- actas/controlador/procesa_censados.php
+++ actas/controlador/procesa_censados.php
@@ -2,6 +2,6 @@
 require_once("../modelo/clasesdeconsulta/class_censados.php");
  /*instanciamos la class que hace la consulta de las personas censadas*/
 $obj = new Personas_censados();             
-$obj->censados();                
+$obj->POST_censados();                
 echo $obj;
  ?>
--- actas/modelo/clasesdeconsulta/class_censados.php
+++ actas/modelo/clasesdeconsulta/class_censados.php
@@ -5,7 +5,7 @@ require_once("../modelo/conecta.php");
 class Personas_censados{
 	private $censadas;
 	
-	public function censados(){
+	public function POST_censados(){
 		//seleccionamos de forma multiple
 		$sql = mysqli_query(Conecta::conx(),"SELECT COUNT(datos_personales.CI, datos_familiares.CEDULA_F) AS censados FROM datos_personales, datos_familiares WHERE datos_personales.CI = 'ci' AND datos_familiares.CEDULA_F = 'cedf[]'") or die('cedula no registrada' .$sql.  mysqli_errno(Conecta::conx()));
 		//$sql .= "";		

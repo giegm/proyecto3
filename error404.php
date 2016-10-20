5d2223e45afb701efd818fd075a4062b40d992bb
Gleiver Gil <Gleiver Gil>
2016-10-20 13:03:01 -0400

    Se elimino la pagina de error 404, asi que se agrego

---
+++ error404.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Error 404</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link href='http://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" href="images/logo_.ico" />
+<?php include_once("analyticstracking.php") ?>
+<style type="text/css">
+body{
+	font-family: 'Love Ya Like A Sister', cursive;
+}
+body{
+	background:#eaeaea;
+}	
+.wrap{
+	margin:0 auto;
+	width:1000px;
+}
+.logo{
+	text-align:center;
+	margin-top:100px;
+}
+.logo h1{
+	font-size:200px;
+	color:#45F4F1;
+	text-align:center;
+	margin-bottom:1px;
+	text-shadow:1px 1px 6px #fff;
+}
+.logo img{
+	width:350px;
+}
+.logo p{
+	color:#272727;
+	font-size:40px;
+	margin-top:1px;
+}	
+.logo p span{
+	color:lightgreen;
+}	
+.sub a{
+	color:#fff;
+	background:#272727;
+	text-decoration:none;
+	padding:10px 20px;
+	font-size:13px;
+	font-family: arial, serif;
+	font-weight:bold;
+	-webkit-border-radius:.5em;
+	-moz-border-radius:.5em;
+	-border-radius:.5em;
+}	
+.footer{
+	color:black;
+	position:absolute;
+	right:10px;
+	bottom:10px;
+}	
+.footer a{
+	color:rgb(114, 173, 38);
+}	
+</style>
+</head>
+
+
+<body>
+	<div class="wrap">
+	   <div class="logo">
+	   
+	   <h1>404</h1>
+	    <p>Error ocurrido!</p><p>Página no encontrada</p>
+	    
+  	      <div class="sub">
+	        <p><a href="index.php">Atras</a></p>
+	      </div>
+        </div>
+	</div>
+</body>
